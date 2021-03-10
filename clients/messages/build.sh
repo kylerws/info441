bdb076425a52f6e80d9a814cbca3877a112925d3
kshruti99 <shrutikompella@gmail.com>
2021-03-09 20:01:09 -0800

    client implementation

--- clients/messages/build.sh
+++
@@ -1,4 +0,0 @@
-#cmd /V /C "set GOOS=linux&&go build"
-#GOOS=linux go build
-docker build -t kshruti/messageserver .
-#docker run -d -p 443:443 --name clientmsg kshruti/messageserver
--- clients/messages/deploy.sh
+++
@@ -1,6 +0,0 @@
-sh build.sh
-docker login
-docker push kshruti/messageserver
-ssh ec2-user@ec2-44-242-96-157.us-west-2.compute.amazonaws.com # < awsScript.sh
-
-#docker run -d -p 443:443 -p 80:80 --name 344gateway -v /etc/letsencrypt:/etc/letsencrypt:ro -e ADDR=:443 -e TLSCERT=/etc/letsencrypt/live/iamshruti.me/fullchain.pem -e TLSKEY=/etc/letsencrypt/live/iamshruti.me/privkey.pem kshruti/summaryServer
