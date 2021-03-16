899a9e2a51e4cd16771068656eb6c059baf47f90
Kyler Sakumoto <kylerws@uw.edu>
2021-03-16 15:51:15 -0700

    Done

--- 441-clients/scripts/dockerize.sh
+++ 441-clients/scripts/dockerize.sh
@@ -1,7 +1,10 @@
-docker pull kylerws/441-client
+# export IMAGE="kylerws/441-client"
+export IMAGE="mackenziehutchison/441-final-client"
+
+docker pull $IMAGE
 
 docker rm -f client
 
-docker run -d -p 80:80 -p 443:443 -v /etc/letsencrypt:/etc/letsencrypt:ro --name client kylerws/441-client
+docker run -d -p 80:80 -p 443:443 -v /etc/letsencrypt:/etc/letsencrypt:ro --name client $IMAGE
 
 exit
